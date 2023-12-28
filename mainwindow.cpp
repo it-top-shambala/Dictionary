e1501d3928c3151b06e6239515bb24f399f22c71
Starinin Andrey <starininandrey@gmail.com>
2023-12-28 19:25:45 +0300

    Add  MainWindow settings

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QScreen>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    this->setWindowFlag(Qt::WindowMinimizeButtonHint, false);
+    this->setWindowFlag(Qt::WindowMinMaxButtonsHint, false);
+    this->setWindowFlag(Qt::WindowSystemMenuHint, false);
+    this->setWindowFlag(Qt::WindowFullscreenButtonHint, false);
+    this->setWindowFlag(Qt::MSWindowsFixedSizeDialogHint);
     ui->setupUi(this);
+
+    move(QGuiApplication::screens().at(0)->geometry().center() - frameGeometry().center());
 }
 
 MainWindow::~MainWindow()
